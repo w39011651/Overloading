22bd5c7c54f3c75c96a506dd4f965bfd745f01b6
s1111537@mail.yzu.edu.tw <s1111537@mail.yzu.edu.tw>
2023-04-26 13:51:14 +0800

    testpushonpubliccomputer

---
+++ Overloading/Source.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+
+using namespace std;
+class Fraction {
+public:
+	Fraction()
+		:num(0), den(0)
+	{
+	}
+	Fraction(int num, int den) {
+		this->num = num;
+		this->den = den;
+	}
+private:
+	int num = 0;//¤À¤l
+	int den = 0;//¤À¥À
+};
+int main() {
+
+}
